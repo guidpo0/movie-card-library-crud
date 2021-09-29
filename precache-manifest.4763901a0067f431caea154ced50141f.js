dd522547da047327da5df24394458ff5ededdb6e
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-28 23:23:38 -0300

    Updates

---
+++ precache-manifest.4763901a0067f431caea154ced50141f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcc838030b3c628f133e0bd25c469d5",
+    "url": "/movie-cards-library-crud/index.html"
+  },
+  {
+    "revision": "b4e69490444ac8678967",
+    "url": "/movie-cards-library-crud/static/css/main.070bb366.chunk.css"
+  },
+  {
+    "revision": "7d9acb44592ae0594ba1",
+    "url": "/movie-cards-library-crud/static/js/2.d28d0443.chunk.js"
+  },
+  {
+    "revision": "b4e69490444ac8678967",
+    "url": "/movie-cards-library-crud/static/js/main.e0c431d3.chunk.js"
+  },
+  {
+    "revision": "2175941fc204fe3b3689",
+    "url": "/movie-cards-library-crud/static/js/runtime-main.ac7a69e8.js"
+  }
+]);
