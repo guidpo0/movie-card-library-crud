1c228e16329b08c8fa6f1efb8989fc19c93b1395
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-29 20:57:03 -0300

    Updates

--- precache-manifest.feb676fa87bc9c08bdf56659521abeb3.js
+++ precache-manifest.feb676fa87bc9c08bdf56659521abeb3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "09ff59587cf05883122c2f38c6be0e81",
+    "revision": "643191557a1b993c2be1ec66a470750b",
     "url": "/movie-cards-library-crud/index.html"
   },
   {
-    "revision": "423debd17d3cf27213dc",
+    "revision": "99ddb488230928660079",
     "url": "/movie-cards-library-crud/static/css/main.35eba271.chunk.css"
   },
   {
-    "revision": "7d9acb44592ae0594ba1",
-    "url": "/movie-cards-library-crud/static/js/2.d28d0443.chunk.js"
+    "revision": "3d2ebc08a2a2ecae28cb",
+    "url": "/movie-cards-library-crud/static/js/2.90ed079f.chunk.js"
   },
   {
-    "revision": "423debd17d3cf27213dc",
-    "url": "/movie-cards-library-crud/static/js/main.5ba96732.chunk.js"
+    "revision": "99ddb488230928660079",
+    "url": "/movie-cards-library-crud/static/js/main.d9776e62.chunk.js"
   },
   {
     "revision": "2175941fc204fe3b3689",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-cards-library-crud/precache-manifest.80d5c3e6aa6d0e680848288a16c969ea.js"
+  "/movie-cards-library-crud/precache-manifest.feb676fa87bc9c08bdf56659521abeb3.js"
 );
 
 self.addEventListener('message', (event) => {
